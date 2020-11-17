0c79e513e6cc9e162d61534abce521d3df6b32ba
Владислав <@->
2020-11-17 14:19:29 +0300

    init

---
+++ WinFormsTimer/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace WinFormsTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.logLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите количество секунд";
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(12, 40);
+            this.timeTextBox.Multiline = true;
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(260, 31);
+            this.timeTextBox.TabIndex = 1;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(12, 77);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(260, 47);
+            this.button.TabIndex = 2;
+            this.button.Text = "Запуск";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.SomeFuncForTimer3);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(12, 166);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.Size = new System.Drawing.Size(260, 31);
+            this.logTextBox.TabIndex = 4;
+            this.logTextBox.Text = "C:\\Users\\Владислав\\Desktop\\log\\log.txt";
+            // 
+            // logLabel
+            // 
+            this.logLabel.AutoSize = true;
+            this.logLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.logLabel.Location = new System.Drawing.Point(8, 139);
+            this.logLabel.Name = "logLabel";
+            this.logLabel.Size = new System.Drawing.Size(252, 24);
+            this.logLabel.TabIndex = 3;
+            this.logLabel.Text = "Введите путь к файлу лога";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 205);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.logLabel);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.Label logLabel;
+    }
+}
+
---
+++ WinFormsTimer/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace WinFormsTimer
+{
+    public partial class Form1 : Form
+    {
+
+        public int timeInSec = 0;
+        private System.Timers.Timer T1;
+        private AutoResetEvent waitHandler = new AutoResetEvent(true);
+        private ManualResetEvent resetEvent = new ManualResetEvent(false);
+        private ManualResetEvent resetEvent2 = new ManualResetEvent(false);
+        public ManualResetEvent resetEvent3 = new ManualResetEvent(false);
+        private System.Windows.Forms.Timer TimerWF = new System.Windows.Forms.Timer();
+        public TimerInfo WinFormsTimerInfo = new TimerInfo("System_Windows_Forms_Timer");
+        Stopwatch stopwatchWinFormsTimer = new Stopwatch();  // Запускаем внутренний таймер объекта Stopwatch
+        //static string writePath = "/home/vladdden/RiderProjects/ConsoleApp1/ConsoleApp1/log.txt";
+        public static string writePath = @"C:\Users\Владислав\Desktop\log\log.txt";
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        public void button_Click(object sender, EventArgs e)
+        {
+            timeInSec = Convert.ToInt32(timeTextBox.Text);
+            writePath = logTextBox.Text;
+            button.Enabled = false;
+
+            Thread t1 = new Thread(System_Timers_Timer);
+            t1.Name = "System_Timers_Timer";
+            t1.Start(timeInSec * 1000);
+
+            Thread t2 = new Thread(System_Threading_Timer);
+            t2.Name = "System_Threading_Timer";
+            t2.Start(timeInSec * 1000);
+
+            
+            System_Windows_Forms_Timer(timeInSec * 1000);
+            /*
+            TimerWF.Interval = timeInSec*1000;
+            TimerWF.Tick += SomeFuncForTimer3;
+            TimerWF.Enabled = true;
+            
+            Thread t3 = new Thread(System_Windows_Forms_Timer);
+            t3.Name = "System_Windows_Forms_Timer";
+            t3.Start(timeInSec * 1000);
+            */
+            MessageBox.Show("Таймеры закончили свою работу, нажмите Enter.");
+            Console.Read();
+            using (StreamWriter endString = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+            {
+                endString.WriteLineAsync("--------------------------------------------------------------------------------------------");
+            }
+        }
+
+        public async void Logging(TimerInfo info)
+        {
+            waitHandler.WaitOne();
+            //string writePath = "" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/log.txt";
+            //@"C:\SomeDir\log.txt";
+
+            string t1 = $"{info.start:HH:mm:ss.fff} Таймер {info.name} запустился ({info.startTicks})";
+            string t2 = $"{info.stop:HH:mm:ss.fff} Таймер {info.name} завершился ({info.stopTicks})";
+            string t3 = $"Время выполнения таймера {info.name} ({timeInSec} сек.) - {info.stop - info.start}({info.difference})";
+            string t4 = $"Во время выполнения таймера {info.name} была обнаружена ошибка - {info.timerException}";
+            string t5 = "--------------------------------------------------------------------------------------------";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    await sw.WriteLineAsync(t1);
+                }
+
+                using (StreamWriter sw2 = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    await sw2.WriteLineAsync(t2);
+                }
+
+                using (StreamWriter sw3 = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    await sw3.WriteLineAsync(t3);
+                }
+
+                if (info.timerException != null)
+                {
+                    using (StreamWriter sw4 = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                    {
+                        await sw4.WriteLineAsync(t4);
+                    }
+                }
+
+                using (StreamWriter sw5 = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    await sw5.WriteLineAsync(t5);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            waitHandler.Set();
+        }
+        private void System_Timers_Timer(object msecVal)
+        {
+            Console.WriteLine("Запуск первого таймера - {0:HH:mm:ss.fff}", DateTime.Now);
+            DateTime start = new DateTime();
+            DateTime stop = new DateTime();
+            long startTicks = 0;
+            long stopTicks = 0;
+            long difference = 0;
+            Exception timerException = null;
+            try
+            {
+                Stopwatch stopwatch = new Stopwatch();  // Запускаем внутренний таймер объекта Stopwatch
+                stopwatch.Start();
+                start = DateTime.Now;
+                startTicks = DateTime.Now.Ticks;
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //Thread.Sleep(100);
+                // Create a timer with a two second interval.
+                T1 = new System.Timers.Timer((int)msecVal);
+                // Hook up the Elapsed event for the timer. 
+                T1.Elapsed += SomeFuncForTimer1;
+                T1.AutoReset = false;
+                T1.Enabled = true;
+                resetEvent.WaitOne(); // This blocks the thread until resetEvent is set
+                resetEvent.Close();
+                resetEvent.Dispose();
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                stopwatch.Stop(); // Останавливаем внутренний таймер объекта Stopwatch
+                stop = DateTime.Now;
+                stopTicks = DateTime.Now.Ticks;
+                difference = stopwatch.ElapsedTicks;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + ":" + e);
+                timerException = e;
+            }
+            finally
+            {
+                Console.WriteLine("Первый таймер завершился.");
+                TimerInfo timer = new TimerInfo(Thread.CurrentThread.Name, start, startTicks, stop, stopTicks, difference, timerException);
+                Logging(timer);
+            }
+        }
+
+        private void System_Threading_Timer(object msecVal)
+        {
+            Console.WriteLine("Запуск второго таймера -  {0:HH:mm:ss.fff}", DateTime.Now);
+            DateTime start = new DateTime();
+            DateTime stop = new DateTime();
+            long startTicks = 0;
+            long stopTicks = 0;
+            long difference = 0;
+            Exception timerException = null;
+            try
+            {
+                Stopwatch stopwatch = new Stopwatch();  // Запускаем внутренний таймер объекта Stopwatch
+                stopwatch.Start();
+                start = DateTime.Now;
+                startTicks = DateTime.Now.Ticks;
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //Thread.Sleep(100);
+                // Create a timer with a two second interval.
+                //TimerCallback tm = new TimerCallback(SomeFunc);
+                System.Threading.Timer T2 = new System.Threading.Timer(SomeFuncForTimer2, null, (int)msecVal, Timeout.Infinite);
+                resetEvent2.WaitOne(); // This blocks the thread until resetEvent is set
+                resetEvent2.Close();
+                resetEvent2.Dispose();
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                stopwatch.Stop(); // Останавливаем внутренний таймер объекта Stopwatch
+                stop = DateTime.Now;
+                stopTicks = DateTime.Now.Ticks;
+                difference = stopwatch.ElapsedTicks;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + ":" + e);
+                timerException = e;
+            }
+            finally
+            {
+                Console.WriteLine("Второй таймер завершился.");
+                TimerInfo timer = new TimerInfo(Thread.CurrentThread.Name, start, startTicks, stop, stopTicks, difference, timerException);
+                Logging(timer);
+            }
+        }
+        
+        private void System_Windows_Forms_Timer(object msecVal)
+        {
+            Console.WriteLine("Запуск третьего таймера -  {0:HH:mm:ss.fff}", DateTime.Now);
+            try
+            {
+                Console.WriteLine(1);
+                stopwatchWinFormsTimer.Start();
+                WinFormsTimerInfo.start = DateTime.Now;
+                WinFormsTimerInfo.startTicks = DateTime.Now.Ticks;
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                Console.WriteLine(2);
+                TimerWF.Interval = (int) msecVal;
+                TimerWF.Tick += SomeFuncForTimer3;
+                TimerWF.Enabled = true;
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                Console.WriteLine(3);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + ":" + e);
+                WinFormsTimerInfo.timerException = e;
+            }
+        }
+        
+        private void SomeFuncForTimer1(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Timer 1 is working!");
+            resetEvent.Set();
+        }
+
+        private void SomeFuncForTimer2(object o)
+        {
+            Console.WriteLine("Timer 2 is working!");
+            resetEvent2.Set();
+        }
+        public void SomeFuncForTimer3(object source, EventArgs e)
+        {
+            Console.WriteLine($"Timer 3 is working!");
+            (source as System.Windows.Forms.Timer).Stop();
+            (source as System.Windows.Forms.Timer).Dispose();
+            stopwatchWinFormsTimer.Stop(); // Останавливаем внутренний таймер объекта Stopwatch
+            WinFormsTimerInfo.stop = DateTime.Now;
+            WinFormsTimerInfo.stopTicks = DateTime.Now.Ticks;
+            WinFormsTimerInfo.difference = stopwatchWinFormsTimer.ElapsedTicks;
+            Logging(WinFormsTimerInfo);
+            //resetEvent3.Set();
+        }
+    }
+
+    public class TimerInfo
+    {
+        public string name;
+        public DateTime start; // ("{0:O}", now)
+        public long startTicks;
+        public DateTime stop;
+        public long stopTicks;
+        public long difference;
+        public Exception timerException = null;
+
+        public TimerInfo(string Name)
+        {
+            name = Name;
+        }
+
+        public TimerInfo(string Name, DateTime Start, long StartTicks, DateTime Stop, long StopTicks, long Difference, Exception TimerException)
+        {
+            name = Name;
+            start = Start;
+            startTicks = StartTicks;
+            stop = Stop;
+            stopTicks = StopTicks;
+            difference = Difference;
+            timerException = TimerException;
+        }
+    }
+}
